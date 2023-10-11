a794aa843925488cdaeba8b81c300e345fca51be
Allen Chou <f1470891079@gmail.com>
2023-10-11 18:05:13 +0800

    Image obj

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,14 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
+const YourComponent = () => (
+  <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+);
 
 export default function FirstPost() {
   return (
@@ -7,6 +17,7 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+      <YourComponent />
     </>
   );
 }

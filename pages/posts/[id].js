2892878193abb47f7069508fda58102454cc7c75
Allen Chou <f1470891079@gmail.com>
2023-10-16 19:45:42 +0800

    Date format

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,23 @@
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+import Head from "next/head";
 import Layout from "../../components/layout";
 
 export default function Post({ postData }) {
   return (
     <Layout>
+      {/* Add this <Head> tag */}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      {/* Replace {postData.date} with this */}
+      <Date dateString={postData.date} />
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
